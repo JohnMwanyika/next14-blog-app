94a637161f144be7d5c6425d00008e7be8173bd2
John Mwanyika <mwanyikajohn@outlook.com>
2024-02-27 19:28:47 +0300

    Fix:Add fix remote images, add blogs template

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,3 +1,21 @@
+import PostCard from '@/components/postCard/postCard';
+import styles from './blog.module.css';
+
 export default function BlogsPage() {
-    return (<div>All Blogs</div>)
+    return (
+        <div className={styles.container}>
+            <div className={styles.post}>
+                <PostCard />
+            </div>
+            <div className={styles.post}>
+                <PostCard />
+            </div>
+            <div className={styles.post}>
+                <PostCard />
+            </div>
+            <div className={styles.post}>
+                <PostCard />
+            </div>
+        </div>
+    )
 }
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -1,7 +1,25 @@
+import Image from 'next/image'
+import styles from './postCard.module.css'
+import Link from 'next/link'
+
 export default function PostCard() {
     return (
         <>
-            <div>PostCard</div>
+            <div className={styles.container}>
+                <div className={styles.top}>
+                    <div className={styles.imgContainer}>
+                        <Image src="https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='' fill />
+                    </div>
+                    <div className={styles.date}>
+                        01.03.2024
+                    </div>
+                </div>
+                <div className={styles.bottom}>
+                    <h1 className={styles.title}>Title</h1>
+                    <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque delectus totam labore voluptates eos asperiores autem, alias veritatis commodi iste numquam tempora, quae quam cumque officiis. Tenetur accusantium non at.</p>
+                    <Link href="/blog/post">Read more</Link>
+                </div>
+            </div>
         </>
     )
 }
